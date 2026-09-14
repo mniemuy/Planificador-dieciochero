aebed210a3c221a1ecc670d836b5c11122e775bf
mniemuy <satorikanai@gmail.com>
2026-09-13 23:04:08 -0300

    Parser para las dependencias terminado

--- Tarea1SistemasOperativos.c
+++ Tarea1SistemasOperativos.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -35,7 +37,7 @@ while(fgets(buffer, sizeof(buffer), f) != NULL){ //mientras haya datos, es cmo u
 
     char* sep = strtok_r(buffer, ":", &save);  //los archivos están separados por un :
     struct datoken pama;//para ir cambiando c/u
-
+    
     while(sep != NULL){        
     
      if(ind == 1){
@@ -52,9 +54,32 @@ while(fgets(buffer, sizeof(buffer), f) != NULL){ //mientras haya datos, es cmo u
         pama.tiempo_ms = atoi(sep);
     printf("PAMAPRINT: %d\n", pama.tiempo_ms);
         ind++;
+    } else if(ind == 4){
+        pama.dependencias = NULL;
+        pama.nd = 0;
+        char* save2 = NULL;
+
+        char* aux =     strtok_r(sep, ",\n", &save2);
+       
+        while(aux != NULL){
+            pama.dependencias = realloc(pama.dependencias, (pama.nd + 1)*sizeof(char*));
+
+            pama.dependencias[pama.nd] = malloc(strlen(aux) + 1);
+            strcpy(pama.dependencias[pama.nd], aux);
+            pama.nd++;
+
+            aux = strtok_r(NULL, ",\n", &save2);
+        }
+        
+            printf("Actividad %s tiene %d dependencias: ", pama.ID_Actividad, pama.nd);
+            for (int i = 0; i < pama.nd; i++) {
+                     printf("[%s] ", pama.dependencias[i]);
+                }
+                printf("\n");
+                    //verificar la verificación (estoy chato basta por favor)
     }
 
-    //tengo q hacer una para el 4 pero tengo q manejar el arreglo de punteros a punteros y lo dejaré pa mañana
+    //lo dejé pa hoy efectivamente
     printf("Separación: %s\n", sep);
 
     sep = strtok_r(NULL, ":", &save);    
